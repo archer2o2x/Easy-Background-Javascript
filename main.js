a01967af64cec15afa3a003781ec4684c420a867
archer2o2x <31862296+archer2o2x@users.noreply.github.com>
2017-09-11 21:27:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var link = prompt("Please Enter A URI :");
-document.body.innerHTML = "";
-var image = document.createElement('img');
-image.setAttribute('src', link);
-image.setAttribute('style', 'width:' + window.innerWidth +'px;height:' + window.innerHeight +'px');
-document.body.appendChild(image);
-document.addEventListener("keydown", function(e) {
-  if (e.keyCode == 13) {
-    image.setAttribute('style', 'width:' + window.innerWidth +'px;height:' + window.innerHeight +'px');
-  }
-}, false);
+var link = prompt("Please Enter A URI :");  
+document.body.innerHTML = "";  
+var image = document.createElement('img');  
+image.setAttribute('src', link);  
+image.setAttribute('style', 'width:' + window.innerWidth +'px;height:' + window.innerHeight +'px');  
+document.body.appendChild(image);  
+document.addEventListener("keydown", function(e) 
+  { if (e.keyCode == 13) 
+    {     
+      image.setAttribute('style', 'width:' + window.innerWidth +'px;height:' + window.innerHeight +'px');    
+    } 
+  }, 
+false);
