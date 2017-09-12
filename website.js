95c3404ae87895fb37c745aa8c5738418a4e0d4d
archer2o2x <31862296+archer2o2x@users.noreply.github.com>
2017-09-12 11:17:49 +0100

    Update website.js

--- website.js
+++ website.js
@@ -1,4 +1,6 @@
 document.body.innerHTML="";
 var page = document.createElement("iframe");
 page.src = prompt("Please Enter Page URL : ")
+page.height = window.innerHeight;
+page.width = window.innerWidth;
 document.body.appendChild(page);
